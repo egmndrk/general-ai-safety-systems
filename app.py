b027c663684156d0a0180167763bc63be095344a
Egemen Doruk Serdar <egemendorukserdar@gmail.com>
2024-12-29 22:40:13 +0300

    general-ai-safety-systems

--- app.py
+++ app.py
@@ -14,27 +14,27 @@ def create_app():
     # Sample PDF information - replace with your actual PDF files
     pdf_files = [
         {
-            'title': 'Report 1 - Project Idea',
-            'filename': 'FKR-ProjectIdea.pdf',
-            'description': 'Introduction and project overview'
+            "title": "Report 1 - Project Idea",
+            "filename": "FKR-ProjectIdea.pdf",
+            "description": "Introduction and project overview"
         },
         {
-            'title': 'Report 2 - Project Specifitaion',
-            'filename': 'ProjectSpecificationsReport.pdf',
-            'description': 'Architectural Design and Workflow'
+            "title": "Report 2 - Project Specification",
+            "filename": "ProjectSpecificationsReport.pdf",
+            "description": "Architectural Design and Workflow"
         },
         {
-            'title': 'Report 3 - Analysis',
-            'filename': 'Analysis_Report_for_General_AI_Safety_Systems.pdf',
-            'description': 'Project Analysis'
+            "title": "Report 3 - Analysis",
+            "filename": "Analysis_Report_for_General_AI_Safety_Systems.pdf",
+            "description": "Comprehensive examination of current and proposed safety systems for school transportation, addressing limitations and introducing AI-driven solutions"
+        },
+        {
+            "title": "Report 4 - High-Level Design Report",
+            "filename": "High-LevelDesignReport.pdf",
+            "description": "Detailed architectural breakdown and system-level design for AI safety systems in school transportation"
         }
     ]
 
-    '''{
-            'title': 'Report 4 - Conclusion',
-            'filename': '',
-            'description': 'Conclusions and future work'
-        }'''
 
     @app.route('/', methods=["GET", "POST"])
     def index():
